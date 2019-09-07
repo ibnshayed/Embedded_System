185f18c42da02c3425472cff3492a5a032ebf814
ibnshayed <emranibnshayed@gmail.com>
2019-09-07 18:50:13 +0600

    Embedded System Summer 2019

--- Eclipse Code/Final_1/Main.c
+++ Eclipse Code/Final_1/Main.c
@@ -5,4 +5,213 @@
  *      Author: ibnshayed
  */
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+#define F_CPU 16000000UL
+
+#define FOSC 16000000 /**< Clock speed for UBRR calculation. refer page 179 of 328p datasheet. */
+#define BAUD 9600 /**< Baud Rate in bps. refer page 179 of 328p datasheet. */
+#define MYUBRR FOSC/16/BAUD-1 /**< UBRR = (F_CPU/(16*Baud))-1 for asynch USART page 179 328p datasheet. Baud rate 9600bps, assuming	16MHz clock UBRR0 becomes 0x0067*/
+
+#define numSlots 20
+#define DELAY 100
+
+int result;
+volatile int count = 0;
+volatile int revolution = 0;
+volatile uint8_t flag = 0;
+/////////////////////USART Start////////////////
+/**
+ * @brief Initialize USART for 8 bit data transmit no parity and 1 stop bit.
+ *
+ *@details This is a code snippet from datasheet page 182
+ *
+ * @param ubrr The UBRR value calculated in macro MYUBRR
+ * @see MYUBRR
+ */
+void USART_init(unsigned int ubrr) {
+
+	UCSR0C = (0 << USBS0) | (3 << UCSZ00); /// Step 1. Set UCSR0C in Asynchronous mode, no parity, 1 stop bit, 8 data bits
+	UCSR0A = 0b00000000; /// Step 2. Set UCSR0A in Normal speed, disable multi-proc
+
+	UBRR0H = (unsigned char) (ubrr >> 8); /// Step 3. Load ubrr into UBRR0H and UBRR0L
+	UBRR0L = (unsigned char) ubrr;
+
+	UCSR0B = 0b00011000; /// Step 4. Enable Tx Rx and disable interrupt in UCSR0B
+}
+
+/**
+ * @brief Send 8bit data.
+ *
+ *@details This is a code snippet from datasheet page 184
+ *
+ * @param data The 8 bit data to be sent
+ */
+
+int USART_send(char c, FILE *stream) {
+
+	while (!(UCSR0A & (1 << UDRE0))) /// Step 1.  Wait until UDRE0 flag is high. Busy Waitinig
+	{
+		;
+	}
+
+	UDR0 = c; /// Step 2. Write char to UDR0 for transmission
+}
+
+/**
+ * @brief Receive 8bit sata.
+ *
+ *@details This is a code snippet from datasheet page 187
+ *
+ * @return Returns received data from UDR0
+ */
+int USART_receive(FILE *stream) {
+
+	while (!(UCSR0A & (1 << RXC0)))
+		/// Step 1. Wait for Receive Complete Flag is high. Busy waiting
+		;
+
+	return UDR0; /// Step 2. Get and return received data from buffer
+}
+////////////////USART END////////////////
+ISR(PCINT0_vect) {
+	if (count < 20) {
+		count++;
+	} else {
+		revolution++;
+		count = 0;
+		flag = 1;
+	}
+
+}
+
+void init_pcint0() {
+	/*DDRD &= ~(1 << DDD2); /// Set int0 as input
+	 PORTD |= (1 << PD2); /// pullup
+	 EICRA |= (1 << ISC01); /// EICRA – External Interrupt Control Register A ; ISC01, ISC00 = 1,1 a Rising edge triggers interrupt
+	 EICRA |= (1 << ISC00);
+	 EIMSK |= (1 << INT0); /// Enable INT0*/
+	DDRB &= ~(1 << PB2);
+	PORTB |= (1 << PB2);
+
+	PCICR |= (1 << PCIE0);
+	PCIFR |= (1 << PCIF0);
+	PCMSK0 |= (1 << PCINT0);
+}
+
+void init_ADC() {
+	/// ADMUX section 23.9.1 page 262
+	///BIT 7 and BIT 6  AVCC with external capacitor at AREF pin REFS0 =0 and REFS1= 1
+	/// BIT 5  ADC Left Adjust Result ADLAR = 0
+	/// BIT 3:0 MUX3:0 0b0000 for channel 0
+	ADMUX = 0b01000000;
+	//same as previous line
+	//	ADMUX = (_BV(REFS0))| (ADMUX & ~_BV(REFS1))|(_BV(ADLAR))|(ADMUX & ~_BV(MUX3))|(ADMUX & ~_BV(MUX2))|(ADMUX & ~_BV(MUX1))|(ADMUX & ~_BV(MUX0));
+
+	///DIDR0  Digital Input Disable Register 0 section Section 23.9.4 page 265 - 266
+	/// Disable digital input buffer of ADC0 to save power consumption
+	//DIDR0 = 0b00000001;
+
+	/// ADSCRA ADC Control and Status Register A Section 23.9.2 page 263 -264
+	///Bit 7  ADEN: ADC Enable =1 enable ADC
+	///Bit 6  ADSC: ADC Start Conversion =0 do not start conversion
+	///Bit 5  ADATE: ADC Auto Trigger Enable = 0 Disable trigger
+	///Bit 4  ADIF: ADC Interrupt Flag = 0
+	///Bit 3  ADIE: ADC Interrupt Enable = 0 Disable ADC interrupt
+	///Bits 2:0  ADPS2:0: ADC Prescaler Select Bits 010 division factor = 4
+	ADCSRA = 0b10000111;
+
+}
+
+uint16_t read_ADC(uint8_t ch) {
+	// select the corresponding channel 0~7
+	// ANDing with ’7′ will always keep the value
+	// of ‘ch’ between 0 and 7
+	ch &= 0b00000111;  // AND operation with 7
+	ADMUX = (ADMUX & 0xF8) | ch; // clears the bottom 3 bits before ORing
+
+	// start single convertion
+	// write ’1′ to ADSC
+	ADCSRA |= (1 << ADSC);
+
+	// wait for conversion to complete
+	// ADSC becomes ’0′ again
+	// till then, run loop continuously
+	while (ADCSRA & (1 << ADSC))
+		;
+
+	return (ADC);
+}
+
+uint8_t graycode_table[64] = { 0b00000000, 0b00000001, 0b00000011, 0b00000010,
+		0b00000110, 0b00000111, 0b00000101, 0b00000100, 0b00001100, 0b00001101,
+		0b00001111, 0b00001110, 0b00001010, 0b00001011, 0b00001001, 0b00001000,
+		0b00011000, 0b00011001, 0b00011011, 0b00011010, 0b00011110, 0b00011111,
+		0b00011101, 0b00011100, 0b00010100, 0b00010101, 0b00010111, 0b00010110,
+		0b00010010, 0b00010011, 0b00010001, 0b00010000, 0b00110000, 0b00110001,
+		0b00110011, 0b00110010, 0b00110110, 0b00110111, 0b00110101, 0b00110100,
+		0b00111100, 0b00111101, 0b00111111, 0b00111110, 0b00111010, 0b00111011,
+		0b00111001, 0b00111000, 0b00101000, 0b00101001, 0b00101011, 0b00101010,
+		0b00101110, 0b00101111, 0b00101101, 0b00101100, 0b00100100, 0b00100101,
+		0b00100111, 0b00100110, 0b00100010, 0b00100011, 0b00100001, 0b00100000
+
+};
+
+int main() {
+
+	init_ADC();
+	USART_init(MYUBRR);
+	stdout = fdevopen(USART_send, NULL);
+	stdin = fdevopen(NULL, USART_receive);
+
+	init_pcint0();
+	sei();
+	/// Enable global interrupt
+	//DDRD |= (1 << DDD5) | (1 << DDD6);
+
+	//Defining angle range;
+	double minAngle[64];
+	double maxAngle[64];
+	double anglePerDivision = 5.625;
+
+	DDRB = 0x3F;
+	PORTB |= (1 << PD5);
+	PORTB &= ~(1 << PD6);
+
+	while (1) {
+		uint8_t i = 0;
+		uint8_t j = 0;
+		result = read_ADC(0b01000000);
+		//printf("{\"adc0%d\":%d}\n", bit_is_set(ADMUX, 0), result);
+		_delay_ms(100);
+
+		for (i = 0; i < 64; i++) {
+			PORTB = graycode_table[i];
+
+			if (i == 0) {
+				minAngle[i] = 0;
+				maxAngle[i] += anglePerDivision;
+			}
+			if(i >0 && i<64) {
+				minAngle[i] = maxAngle[i - 1];
+				maxAngle[i] += anglePerDivision;
+
+			}
+			if (result == graycode_table[i]) {
+				printf("Range is %.3lf degree to %.3lf degree.", minAngle[i],
+						maxAngle[i]);
+				_delay_ms(DELAY);
+				break;
+			}
+
+		}
+
+
+
+
+	}
+}
 
--- Eclipse Code/Final_2/Main.c
+++ Eclipse Code/Final_2/Main.c
@@ -6,3 +6,207 @@
  */
 
 
+/**
+ * @file int0_tmr1_328p.c
+ * @author rezwan
+ * @date 09.08.2015
+ *
+ * @brief AVR ATMEGA328P Timer1 example example for CSE3023.
+ *
+ *  A simple program measures elapsed time between two interrupts. We are using wheel disc encoder to generate interrupt. This encoder outputs low when there is no obstacle and high there is one. That's why the Int0 interrupts at rising edge.
+ *  Demonstrates Timer1 calculation, and ISR. Also demonstrates printf() functionality.
+ *  Eclipse may cause problem in macro resolution = 'Symbol abc could not be resolved'.
+ *  If someone encounters this problem following steps should resolve the matter.
+ *  In the project explorer right click on the project then go to 'index' and left click on Rebuild'
+ *
+ * For more timer tutorial go to http://www.avrfreaks.net/forum/tut-c-newbies-guide-avr-timers?page=all
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <stdio.h>
+#include <inttypes.h> // to print uint32_t
+
+#define SLOTS_PER_REV 20 /**< Total slots in a encoder disc */
+#define FOSC 16000000 /**< Clock speed for UBRR calculation. refer page 179 of 328p datasheet. */
+#define BAUD 57600 /**< Baud Rate in bps. refer page 179 of 328p datasheet. */
+#define MYUBRR FOSC/16/BAUD-1 /**< UBRR = (F_CPU/(16*Baud))-1 for asynch USART page 179 328p datasheet.*/
+
+volatile uint32_t revolution_count;  /**< Counter for revolution */
+volatile int slot_count; /**< Counter for slots */
+volatile uint32_t n; /**< n to count number of times TCNT1 overflowed */
+volatile uint32_t elapse_time=0;/**< Stores cumulative revolution completion time */
+volatile uint8_t flag = 0, print_flag=0;
+volatile int count = 0;
+/**
+ * @brief Initialize USART for 8 bit data transmit no parity and 1 stop bit.
+ *
+ *@details This is a code snippet from datasheet page 182
+ *
+ * @param ubrr The UBRR value calculated in macro MYUBRR
+ * @see MYUBRR
+*/
+void USART_init(unsigned int ubrr)
+{
+
+	UCSR0C = (0<<USBS0)|(3<<UCSZ00); /// Step 1. Set UCSR0C in Asynchronous mode, no parity, 1 stop bit, 8 data bits
+	UCSR0A = 0b00000000;/// Step 2. Set UCSR0A in Normal speed, disable multi-proc
+
+	UBRR0H = (unsigned char)(ubrr>>8);/// Step 3. Load ubrr into UBRR0H and UBRR0L
+	UBRR0L = (unsigned char)ubrr;
+
+
+	UCSR0B = 0b00001000;/// Step 4. Enable Tx Rx and disable interrupt in UCSR0B
+}
+
+/**
+ * @brief Send 8bit data.
+ *
+ *@details This is a code snippet from datasheet page 184
+ *
+ * @param c The 8 bit data to be sent
+ * @param FILE *stream to receive
+*/
+
+int USART_send(char c, FILE *stream)
+{
+
+
+	while ( !( UCSR0A & (1<<UDRE0)) )/// Step 1.  Wait until UDRE0 flag is high. Busy Waitinig
+	{;}
+
+	UDR0 = c; /// Step 2. Write char to UDR0 for transmission
+	return 0;
+}
+
+/**
+ * @brief Receive 8bit data.
+ *
+ *@details This is a code snippet from datasheet page 187
+ *
+ * @return Returns received data from UDR0
+*/
+int USART_receive(FILE *stream)
+{
+
+while ( !(UCSR0A & (1<<RXC0)) )/// Step 1. Wait for Receive Complete Flag is high. Busy waiting
+;
+
+return UDR0;/// Step 2. Get and return received data from buffer
+}
+
+
+/**
+ * @brief Interrupt service routine for Timer1 Overflow vector.
+ *
+ *@details It increases the value of n by one to number of time TIMER1 overflowed between two INT0 interrupts
+ *
+*/
+ISR(TIMER1_OVF_vect)
+{
+
+	n++;
+}
+// ISR(BADISR_vect)
+// {
+//	int i;
+//	i++;
+// }
+void timer1_init()
+{
+
+	TCCR1A = 0b00000000; ///Step 1.  normal mode
+	TCCR1B = 0b00000011; ///Step 2. 1:64 prescaler, internal clock
+	TIMSK1 = 0b00000001; ///Step 3. enable Timer 1 overflow interrupt
+}
+
+/**
+ * @brief Interrupt service routine for INT0 vector.
+ *
+ *@details INT0 interrupt
+ *
+*/
+ISR(INT0_vect)
+{
+	if (flag ==0) /// Exceuted only once for very first external interrupt and initites timer timer counter
+	{
+		TCNT1 =0;
+		n = 0;
+		flag =1; /// Make the flag 1 so that it never enters again
+	}
+	if (slot_count < SLOTS_PER_REV) /// smaller than total slots
+	{
+		slot_count ++; /// increase counter
+	}
+	else /// Completed one revolution
+	{
+		count++;
+		revolution_count ++; /// increase revolution counter
+		print_flag=1; /// tell main loop that it may print new value
+		slot_count =0; /// reset to start count for next revolution
+		elapse_time += n * 262144 + (uint32_t) (TCNT1*4); /// Caclulate the elapsed time  (Prescaler * 2^16)/(Fosc) = 64 * 2^16 / 16MHz = 262144 uS. TCNT1 counter updates after = Prescaler/Fosc = 64/16MHz = 4uS
+		TCNT1 =0; /// reset counter to calculate next revolution
+		n = 0;
+	}
+}
+
+void accurate_blocking(){
+	//accurate blocking/without interrupt/using timer counter
+	//code will be written
+
+}
+
+void accurate_non_blocking(){
+	//accurate non-blocking/without interrupt/using timer counter
+		//code will be written
+}
+
+int microseconds(int value){
+	return value / 1000000;
+}
+int milliseconds(int value){
+	return value / 1000;
+}
+
+int seconds(int value){
+	return value;
+}
+
+
+int main()
+{
+	//our group member's last 2 digit of ID number
+	int a = 30;
+	int b = 42;
+	int c = 62;
+
+	slot_count =0;
+	revolution_count = 0;
+	USART_init(MYUBRR); /// Initialize USART
+	stdout =fdevopen(USART_send,NULL); /// setup printf()
+
+	DDRD &= ~(1<<DDD2);/// Set int0 as input
+
+
+
+	EICRA |= (1<<ISC01); /// EICRA  External Interrupt Control Register A ; ISC01, ISC00 = 1,1 a Rising edge triggers an  interrupt
+	EICRA |= (1<<ISC00);
+	EIMSK|= (1<<INT0);/// Enable INT0
+	timer1_init(); /// Initiate Timer1
+
+	sei();/// Enable global interrupt
+	while(1)
+	{
+
+		if(print_flag==1) /// If print_flag is high print the values
+		{
+			print_flag =0; /// Make print flag 0 so that it will print only when next revolution is complete
+//			cli();
+			printf("Number of revolution is %"PRId32" time taken %"PRId32" uS \r\n", revolution_count,elapse_time);
+//			sei();
+		}
+		//wait for interrupt and do nothing
+	};
+	printf("end\n\r");
+}
